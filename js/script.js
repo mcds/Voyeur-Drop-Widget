2f75a38819105f2fedfa9c23f966c7b47a8525d7
coreyslavnik <corey_slavnik@sympatico.ca>
2010-11-17 13:20:29 -0500

    simplified snippet, add useReferer param

--- js/script.js
+++ js/script.js
@@ -51,44 +51,14 @@ function saveValues() {
   voyeurWidth = $('#voyeur_width').attr('value');
   voyeurHeight = $('#voyeur_height').attr('value');
   voyeurTool = $('#voyeur_tool').val();
-  allowAutoReveal = $('#allow_auto_reveal').attr('checked');
-  allowUser = $('#allow_user').attr('checked');
 }
 
 // Generates the code to place in the generated code textarea (#codeOutput)
 function generateHTML() {
   saveValues();
-  var generatedCode = '<div id="voyeurContainer" style="width: '+ voyeurWidth +'; margin: 0px auto 0px auto; text-align:center;">' + '\r\n';
-  generatedCode += '<img id="voyeur_logo" title="Voyeur - Reveal your Texts" src="http://coreyslavnik.com/voyeur/voyeur.png" />' + '\r\n';
-  generatedCode += '<iframe id="voyeur_iframe" style="display:none;" width="'+ voyeurWidth +'" height="'+ voyeurHeight +'" src=""><p>Your browser does not support iframes - Voyeur will not run</p></iframe><br />' + '\r\n';
-  if (allowUser == true) {
-    generatedCode += 'Tool:&nbsp;&nbsp;' + '\r\n';
-    generatedCode += '<select id="voyeur_tool" name="voyeur_tool" title="Tool">' + '\r\n';
-    generatedCode += '<option id="Bubbles" value="Bubbles"'; generatedCode += (voyeurTool == 'Bubbles') ? ' selected="selected"' : ''; generatedCode += '>Bubbles</option>' + '\r\n';
-    generatedCode += '<option id="Cirrus" value="Cirrus"'; generatedCode += (voyeurTool == 'Cirrus') ? ' selected="selected"' : ''; generatedCode += '>Cirrus</option>' + '\r\n';
-    generatedCode += '<option id="CorpusTypeFrequenciesGrid" value="CorpusTypeFrequenciesGrid"'; generatedCode += (voyeurTool == 'CorpusTypeFrequenciesGrid') ? ' selected="selected"' : ''; generatedCode += '>Frequency Grid</option>' + '\r\n';
-    generatedCode += '<option id="Links" value="Links"'; generatedCode += (voyeurTool == 'Links') ? ' selected="selected"' : ''; generatedCode += '>Links</option>' + '\r\n';
-    generatedCode += '<option id="Reader" value="Reader"'; generatedCode += (voyeurTool == 'Reader') ? ' selected="selected"' : ''; generatedCode += '>Reader</option>' + '\r\n';
-    generatedCode += '<option id="CorpusSummary" value="CorpusSummary"'; generatedCode += (voyeurTool == 'CorpusSummary') ? ' selected="selected"' : ''; generatedCode+= '>Summary</option>' + '\r\n';
-    generatedCode += '<option id="WordCountFountain" value="WordCountFountain"'; generatedCode += (voyeurTool == 'WordCountFountain') ? ' selected="selected"' : ''; generatedCode += '>Word Count Fountain</option>' + '\r\n';
-    generatedCode += '</select><br />' + '\r\n';
-  }
-  if ((allowUser == true) || (allowAutoReveal == false && allowUser == false)) {
-    generatedCode += '<input type="button" id="voyeur_reveal" value="Reveal" onclick="launchVoyeur(\''+voyeurTool+'\','+allowUser+');" />' + '\r\n';
-  }
-  generatedCode += '</div>' + '\r\n';
-
-  generatedCode += '<script type="text/javascript">' + '\r\n';
-  if (allowAutoReveal == true) {
-    generatedCode += 'launchVoyeur(\''+voyeurTool+'\','+allowUser+');' + '\r\n';
-  }
-  generatedCode += 'function launchVoyeur(voyeurTool, allowUser) {' + '\r\n';
-  generatedCode += 'document.getElementById("voyeur_logo").setAttribute("style", "display:none;");' + '\r\n';
-  generatedCode += 'document.getElementById("voyeur_iframe").removeAttribute("style");' + '\r\n';
-  generatedCode += 'if (allowUser == true) {' + '\r\n';
-  generatedCode += 'voyeurTool = document.getElementById("voyeur_tool").options[document.getElementById("voyeur_tool").selectedIndex].value;' + '\r\n' + '}' + '\r\n';
-  generatedCode += 'document.getElementById("voyeur_iframe").setAttribute("src", "http://voyeurtools.org/tool/"+voyeurTool+"/?input="+document.URL);' + '\r\n' + '}' + '\r\n';
-  generatedCode += '</script><br />';
+  var generatedCode = '<script type="text/javascript">' + '\r\n';
+  generatedCode += 'document.write(\'<iframe id="voyeur_iframe" width="'+ voyeurWidth +'" height="'+ voyeurHeight +'" src="http://voyeurtools.org/tool/'+ voyeurTool +'/?useReferer=true"></iframe>\');' + '\r\n';
+  generatedCode += '</script>' + '\r\n';
   
   $('#codeOutput').attr("value", generatedCode);
   $('#afterGenerate').text('Copy the text above and paste it in your HTML code.');
