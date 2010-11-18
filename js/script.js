f545db309bc4543971969d4303724256465f61ae
coreyslavnik <corey_slavnik@sympatico.ca>
2010-11-18 11:33:20 -0500

    add "remove function words" option

--- js/script.js
+++ js/script.js
@@ -46,17 +46,32 @@ $(document).ready(function() {
   });
 });
 
+// Finds when the tool select list is clicked to update options.
+function toolClick() {
+  var currentTool = $('#voyeur_tool').val();
+  if (currentTool == 'Bubbles' || currentTool == 'WordCountFountain') {
+    $('#remove_func_words_tr').fadeOut(250);
+  } else {
+    $('#remove_func_words_tr').fadeIn(250);
+  }
+}
+
 // Saves the values of user input.
 function saveValues() {
   voyeurWidth = $('#voyeur_width').attr('value');
   voyeurHeight = $('#voyeur_height').attr('value');
   voyeurTool = $('#voyeur_tool').val();
+  removeFuncWords = $('#remove_func_words').attr('checked');
 }
 
 // Generates the code to place in the generated code textarea (#codeOutput)
 function generateHTML() {
   saveValues();
-  var generatedCode = '<iframe id="voyeur_iframe" width="'+ voyeurWidth +'" height="'+ voyeurHeight +'" src="http://voyeurtools.org/tool/'+ voyeurTool +'/?useReferer=true"></iframe>' + '\r\n';
+  var generatedCode = '<iframe id="voyeur_iframe" width="'+ voyeurWidth +'" height="'+ voyeurHeight +'" src="http://voyeurtools.org/tool/'+ voyeurTool +'/?useReferer=true';
+  if (removeFuncWords == true && (voyeurTool != 'Bubbles' && voyeurTool != 'WordCountFountain')) {
+    generatedCode += '&stopList=stop.en.taporware.txt';
+  }
+  generatedCode += '"></iframe>' + '\r\n';
   
   $('#codeOutput').attr("value", generatedCode);
   $('#afterGenerate').text('Copy the text above and paste it in your HTML code.');
